5cf82a19815fa66e7b117939b18858832c0d353d
Jeiojey <signofaries@gmail.com>
2022-06-26 17:34:55 +0300

    Homework ready

---
+++ S6Task01/Program.cs
@@ -0,0 +1,35 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 4
+
+using System;
+using static System.Console;
+Clear();
+
+WriteLine("Введите числа через запятую: ");
+int[] array = GetArrayFromString(ReadLine());
+WriteLine($"[{String.Join(",", array)}]");
+WriteLine(CountPositivDigits(array));
+
+int[] GetArrayFromString(string arrStr)
+{
+    string[] arS = arrStr.Split(',', StringSplitOptions.RemoveEmptyEntries);
+    int[] result = new int[arS.Length];
+    for (int i = 0; i < arS.Length; i++)
+    {
+        result[i] = int.Parse(arS[i]);
+    }
+    return result;
+
+}
+
+int CountPositivDigits(int[] newArray)
+{
+    int count = 0;
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        if (newArray[i] > 0) count++;
+    }
+    return count;
+}
+
---
+++ S6Task02/Program.cs
@@ -0,0 +1,28 @@
+﻿//Напишите программу, которая найдёт точку пересечения двух прямых,
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+//значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5) // ответ в примере неверный
+
+using System;
+using static System.Console;
+Clear();
+
+WriteLine("Введите параметр b1: ");
+int b1 = int.Parse(ReadLine());
+WriteLine("Введите параметр k1: ");
+int k1 = int.Parse(ReadLine());
+WriteLine("Введите параметр b2: ");
+int b2 = int.Parse(ReadLine());
+WriteLine("Введите параметр k2: ");
+int k2 = int.Parse(ReadLine());
+FindIntersection(b1, k1, b2, k2);
+
+void FindIntersection(int n1, int n2, int n3, int n4)
+{
+    string point = string.Empty;
+    double X = (n3 - n1) / (n2 - n4);
+    double Y = n2 * X + n1;
+    WriteLine(point = $"({X};{Y})");
+}
+
+
